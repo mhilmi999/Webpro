51b6250454b2cda54376ab065103d9ffe3271b8d
Muhammad Hilmi <mhirumi99@icloud.com>
2020-10-22 19:10:26 +0700

    Massive Update Part 1

--- app/controllers/Panitia.php
+++ app/controllers/Panitia.php
@@ -4,6 +4,12 @@ class Panitia extends Controller
 {
     public function index()
     {
+        $this->view('common/header_panitia');
         $this->view('panitia/index');
+        $this->view('common/footer_panitia');
+    }
+
+    public function ktiMasuk(){
+        
     }
 }
--- app/controllers/Peserta.php
+++ app/controllers/Peserta.php
@@ -2,6 +2,9 @@
 
 class Peserta extends Controller {
     public function index() {
+
+        $this->view('common/header_peserta');
         $this->view('peserta/index');
+        $this->view('common/footer_peserta');
     }
 }
