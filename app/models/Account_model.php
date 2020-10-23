0936a015b102c9c7773933c910a5c837e7fca018
Muhammad Hilmi <mhirumi99@icloud.com>
2020-10-23 15:07:51 +0700

    update siang ini

--- app/models/Account_model.php
+++ app/models/Account_model.php
@@ -7,58 +7,17 @@ class Account_model{
         $this->db = new Database;
     }
 
-    public function register($data)
-    {
-        $this->db->query('INSERT INTO users (username, email, password) VALUES(:username, :email, :password)');
+   function insertNewuser(){
+       $roles = $this->input->post('roles');
 
-        //Bind values
-        $this->db->bind(':username', $data['username']);
-        $this->db->bind(':email', $data['email']);
-        $this->db->bind(':password', $data['password']);
-
-        //Execute function 
-        if($this->db->execute()){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    public function login($username, $password)
-    {
-        $this->db->query('SELECT * FROM users WHERE username = :username');
-
-        //Bind Value
-        $this->db->bind(':username', $username);
-
-        $row = $this->db->single();
-
-        $hashedPassword = $row->passsord;
-
-        if(password_verify($password, $hashedPassword))
-        {
-            return $row;
-        }else{
-            return false;
-        }
-    }
-
-    public function findUserByEmail($email)
-    {
-        //cek email
-        $this->db->query('SELECT * FROM users WHERE email = :email');
+       //Daftarkan user di tabel users
+       $q = "INSERT INTO users (nama, username, password, email)
+            VALUES ('".$this->input->post('nama')."','"
+            .$this->input->post('username')."',MD5('"
+            .$this->input->post('password')."'),'"
+            .$this->input->post('email')."')";
         
-        //parameter email akan di binded dengan variable email
-        $this->db->bind(':email', $email);
-
-        //Jika email terdaftar maka
-        if($this->db->rowCount() > 0)
-        {
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
+        $this->db->query($q);
+   }
 
 }
